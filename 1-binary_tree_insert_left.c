8815a2182b54430f5e87a60a835a22e3f2a8c20a
bek2518 <bereketb20@hotmail.com>
2022-12-13 10:24:17 +0300

    Added the binary tree node function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,10 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	tempNode->parent = parent;
-	tempNode->n = value;
-	tempNode->left = NULL;
-	tempNode->right = NULL;
+	tempNode = binary_tree_node(parent, value);
 
 	if (tempNode == NULL)
 	{
