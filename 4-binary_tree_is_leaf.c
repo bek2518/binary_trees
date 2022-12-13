fb150ec2973f7f6a2c8c7d71332851af3cbf5736
bek2518 <bereketb20@hotmail.com>
2022-12-13 11:32:44 +0300

    Bettystyle

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,12 +7,12 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->left != NULL || node->right != NULL) 
+	if (node == NULL)
 	{
 		return (0);
 	}
 
-	if (node == NULL)
+	else if (node->left != NULL || node->right != NULL)
 	{
 		return (0);
 	}
