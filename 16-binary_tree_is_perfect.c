5560180ffc39bec4350be9b302c284c57195e11d
bek2518 <bereketb20@hotmail.com>
2022-12-14 11:38:26 +0300

    Changed height with size

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,47 +14,43 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 
-	counter_left = binary_tree_height(tree->left);
-	counter_right = binary_tree_height(tree->right);
+	counter_left = binary_tree_size(tree->left);
+	counter_right = binary_tree_size(tree->right);
 
-	counter = counter_left - counter_right;
-	if (counter != 0)
+	if (counter_left == counter_right)
 	{
-		return (0);
-	}
-
-	return (1);
-}
-
-/**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
- * Return: 0 if tree is NULL
- 
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t counter_left, counter_right, counter;
-
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-	{
-		return (0);
-	}
-
-	counter_left = binary_tree_height(tree->left);
-	counter_right = binary_tree_height(tree->right);
-
-	if (counter_left > counter_right)
-	{
-		counter = counter_left;
-	}
-	else if (counter_left < counter_right)
-	{
-		counter = counter_right;
+		counter = counter_left + counter_right + 1;
 	}
 	else
 	{
-		counter = counter_left;
+		counter = 0;
 	}
-	return (counter + 1);
+
+	if (counter != 0)
+	{
+		return (1);
+	}
+
+	return (0);
 }
-*/
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t counter_left = 0, counter_right = 0, counter = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	counter_left = binary_tree_size(tree->left);
+	counter_right = binary_tree_size(tree->right);
+
+	counter = counter_left + counter_right + 1;
+	return (counter);
+}
