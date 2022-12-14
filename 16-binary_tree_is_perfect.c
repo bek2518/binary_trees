6571d8131d3e4fa85f051d9e3ac9f3800d17f6cb
bek2518 <bereketb20@hotmail.com>
2022-12-14 11:41:20 +0300

    Modified size name in perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_size_mod - Measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: 0 if tree is NULL
+ */
+size_t binary_tree_size_mod(const binary_tree_t *tree)
+{
+	size_t counter_left = 0, counter_right = 0, counter = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	counter_left = binary_tree_size_mod(tree->left);
+	counter_right = binary_tree_size_mod(tree->right);
+
+	counter = counter_left + counter_right + 1;
+	return (counter);
+}
+
+
+/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  * Return: 0 if the tree is NULL
@@ -14,8 +36,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 
-	counter_left = binary_tree_size(tree->left);
-	counter_right = binary_tree_size(tree->right);
+	counter_left = binary_tree_size_mod(tree->left);
+	counter_right = binary_tree_size_mod(tree->right);
 
 	if (counter_left == counter_right)
 	{
@@ -32,25 +54,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	}
 
 	return (0);
-}
-
-/**
- * binary_tree_size - Measures the size of a binary tree
- * @tree: pointer to the root node of the tree to measure the size
- * Return: 0 if tree is NULL
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	size_t counter_left = 0, counter_right = 0, counter = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-
-	counter_left = binary_tree_size(tree->left);
-	counter_right = binary_tree_size(tree->right);
-
-	counter = counter_left + counter_right + 1;
-	return (counter);
 }
